812faca8a715e31b575d366531f2df64cc233a7f
Aditya Pratap Saha <41848625+A-d-i-t-y-a-2-5@users.noreply.github.com>
2021-12-22 20:17:04 +0530

    Added functions
    Added sort functions

--- js/function.js
+++ js/function.js
@@ -1,3 +1,4 @@
+$(document).ready(function(){
 $.get('https://raw.githubusercontent.com/A-d-i-t-y-a-2-5/pkmdb/main/Pokedexv1.csv', function(data) {
 
     var html = '<table border="1" cellpadding="2" cellspacing="0" style="border-collapse: collapse" width="100%">\n';
@@ -25,13 +26,37 @@ $.get('https://raw.githubusercontent.com/A-d-i-t-y-a-2-5/pkmdb/main/Pokedexv1.cs
 
     html += "</table>";
     $('#container').append(html);
-});
 
-$(document).ready(function(){
+    $("th").click(function(){
+        var table = $(this).parents('table').eq(0);
+        var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()));
+
+        //no idea about asc property
+        this.asc = !this.asc
+        if (!this.asc){rows = rows.reverse()}
+        for (var i = 0; i < rows.length; i++)
+        {
+            table.append(rows[i]);
+        }
+    });
+
+    function comparer(index) {
+    return function(a, b) {
+        var valA = getCellValue(a, index), valB = getCellValue(b, index);
+        return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB);
+        }
+    }
+
+    function getCellValue(row, index) { 
+        return $(row).children('td').eq(index).text(); 
+    }
+
+});
     $('#myInput').keyup(function(){
         var value = $(this).val().toLowerCase();
         $('tr:gt(0)').filter(function(){
             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
         });
     });
+
 });
